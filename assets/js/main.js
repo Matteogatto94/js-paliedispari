f12fcf86cb2c6c961e7a35e4c53e898a1e747ba8
Matteogatto94 <cosenza94@live.it>
2022-10-14 12:50:39 +0200

    add function palindrome

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+//Palidroma
+//Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+//Pari e Dispari
+
+//L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+const parola = prompt('Inserisci una parola')
+
+const parolaInversa = invertiParola(parola);
+
+if(parola == parolaInversa){
+    console.log(parola);
+    console.log('La parola da lei inserita è palindroma');
+  } else {
+    console.log(parola);
+    console.log('La parola da lei inserita non è palindroma');
+  }
+  
+function invertiParola(str){
+  const strInversa = str.split('').reverse().join('');  
+  return strInversa;
+}
