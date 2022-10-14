3831d120d798912970d0b2e78ee236f71e0981c4
Matteogatto94 <cosenza94@live.it>
2022-10-14 14:47:03 +0200

    add prompt for know number, pc number

---
+++ Pari e Dispari/assets/js/main.js
@@ -0,0 +1,16 @@
+//L’utente sceglie pari o dispari 
+
+const sceltaUtente = prompt('Scegli tra Pari e Dispari')
+
+//inserisce un numero da 1 a 5. 
+
+const sceltaUtenteNumero = Number(prompt('Scegli un numero da 1 a 5'));
+
+//Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+
+const pcNumber = pcRandomNumber(1,5);
+ 
+//Sommiamo i due numeri 
+//Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
+//Dichiariamo chi ha vinto.
+
